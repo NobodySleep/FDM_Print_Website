43906b228826494a49874fae747c972b79cfb084
Tai Nguyen <tainguyen24390@gmail.com>
2026-02-16 20:30:43 -0600

    edit_1

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ toggleBtn.addEventListener("click", () => {
     toggleBtn.textContent = "Night";
   }
 });
+
+const container = document.querySelector('.container');
+document.querySelector('.slider').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%`);
+})
